395d3615e1d94e12bc924ef3d3b078eee4a64794
NekodaMushi <4252@holbertonschool.com>
2022-11-25 11:17:43 +0000

    Back to origin

--- models/city.py
+++ models/city.py
@@ -1,19 +1,11 @@
 #!/usr/bin/python3
-""" City Module for HBNB project """
-from os import getenv
-from models.base_model import BaseModel, Base
-from models.state import State
-import models
-from sqlalchemy import Column, String, Integer, ForeignKey
-from sqlalchemy.orm import relationship
+"""This module defines a class User"""
+from models.base_model import BaseModel
 
 
-class City(BaseModel, Base):
-    """ The city class, contains state ID and name """
-    __tablename__ = 'cities'
-    if getenv('HBNB_TYPE_STORAGE') == 'db':
-        name = Column(String(128), nullable=False)
-        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
-    else:
-        name = ''
-        state_id = ''
+class User(BaseModel):
+    """This class defines a user by various attributes"""
+    email = ''
+    password = ''
+    first_name = ''
+    last_name = ''
